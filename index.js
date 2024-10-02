f5f97b7667a87d9bbe952100a687e43803931e64
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-10-02 15:53:07 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+console.log(findMatching(drivers, 'Bobby'))
+
+
+function fuzzyMatch(drivers, members) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(members.toLowerCase()));
+}
+console.log(fuzzyMatch(drivers, 'Sa'))
+
+
+const drivers = [
+    { name: 'Bobby', hometown: 'Pittsburgh' },
+    { name: 'Sammy', hometown: 'New York' },
+    { name: 'Sally', hometown: 'Cleveland' },
+    { name: 'Annette', hometown: 'Los Angeles' },
+    { name: 'Bobby', hometown: 'Tampa Bay' }
+];
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
+console.log(matchName(drivers, 'Bobby'))
